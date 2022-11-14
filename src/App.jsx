11657ca12782e966cb9c86b2c64ad6a3cc990956
laurismelderis <lauris.melderis77@gmail.com>
2022-11-14 23:09:09 +0200

    Add dynamic donut and inventory update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import Web3 from 'web3'
 import vendingMachineContract from './blockchain/vending.js'
 import { useState, useEffect } from 'react';
 
+const DONUT_PRICE = 0.0001 // in ether
+
 function App() {
   const [web3, setWeb3] = useState(null)
   const [account, setAccount] = useState(null)
   const [vmContract, setVmContract] = useState(null)
-  const [inventory, setInventory] = useState('')
+  const [inventory, setInventory] = useState(0)
   const [myDonutCount, setMyDonutCount] = useState('Register first')
-  const [buyCount, setBuyCount] = useState('')
+  const [buyCount, setBuyCount] = useState(0)
   const [successMessage, setSuccessMessage] = useState('')
+
 
   const connectWalletHandler = async () => {
     if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
@@ -31,6 +34,26 @@ function App() {
       console.log('Please install MetaMask!')
     }
   }
+
+  useEffect(() => {
+    const establishConnection = async () => {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' })
+        const newWeb3 = new Web3(window.ethereum)
+        setWeb3(newWeb3)
+  
+        const accounts = await newWeb3.eth.getAccounts()
+        setAccount(accounts[0])
+  
+        const vm = vendingMachineContract(newWeb3)
+        setVmContract(vm)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    establishConnection()
+  }, [])
 
   useEffect(() => {
     if (vmContract) getInventoryHandler()
@@ -55,9 +78,11 @@ function App() {
     try {
       await vmContract.methods.purchase(buyCount).send({
         from: account,
-        value: web3.utils.toWei('0.0001', 'ether') * buyCount
+        value: web3.utils.toWei(toString(DONUT_PRICE), 'ether') * buyCount
       })
       setSuccessMessage(`Successfully bought ${buyCount} donuts!`)
+      setInventory(Number(inventory) - buyCount)
+      setMyDonutCount(Number(myDonutCount) + buyCount)
     } catch (error) {
       console.log(error.message)
     }
@@ -68,40 +93,45 @@ function App() {
       <h1>Vending machine app</h1>
       <nav className='navbar'>
         <div className="container">
-          <div className="navbar-brand">
-            <h1>Vending machine!</h1>
-          </div>
-          <div className="navbar-end">
+          {!account && <div className="navbar-end">
             <button className="button is-primary" onClick={connectWalletHandler}>Connect Wallet</button>
-          </div>
+          </div>}
         </div>
       </nav>
-      <section>
-        <div className="container">
-          <p>Vending machine inventory: {inventory}</p>
-        </div>
-      </section>
-      <section>
-        <div className="container">
-          <p>My donuts: {myDonutCount}</p>
-        </div>
-      </section>
-      <section>
-        <div className="container">
-          <div className="field">
-            <label className="label">Buy donuts</label>
-            <div className="control">
-              <input type="text" className="input" placeholder="Enter amount ..." onChange={updateDonutCount} />
-            </div>
-            <button className="button is-primary" onClick={buyDonutsHandler}>Buy</button>
+      {account && <>
+        <section>
+          <div className="container">
+            <p>Vending machine inventory: {inventory}</p>
           </div>
-        </div>
-      </section>
-      <section>
-        <div className="container">
-          <p>{successMessage}</p>
-        </div>
-      </section>
+        </section>
+          <section>
+            <div className="container">
+              <p>My donuts: {myDonutCount}</p>
+            </div>
+          </section>
+          <section>
+            <div className="container">
+              <div className="field">
+                <label className="label">Buy donuts (Price per donut {DONUT_PRICE})</label>
+                <div className="control">
+                  <input type="text" className="input" placeholder="Enter amount ..." onChange={updateDonutCount} />
+                </div>
+                <button className="button is-primary" onClick={buyDonutsHandler}>Buy</button>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="container">
+              <p>Price: {buyCount * DONUT_PRICE}</p>
+            </div>
+          </section>
+          <section>
+            <div className="container">
+              <p>{successMessage}</p>
+            </div>
+          </section>
+      </>
+}
     </>
   );
 }
--- src/blockchain/vending.js
+++ src/blockchain/vending.js
@@ -1,7 +1,7 @@
 const abi =  [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 
 const vendingMachineContract = web3 => {
-    return new web3.eth.Contract(abi, "0x12560B980bD0c337B3cD720C58Cb06ae364643db") // Second parameter is the deployed contract address
+    return new web3.eth.Contract(abi, "0x021CcAeDc8ef165907959a131568AD6D72778f29") // Second parameter is the deployed contract address
 }
 
 export default vendingMachineContract
